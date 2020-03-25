4683feb36ba17ad4b4c54a4279feed2a62d90f3e
ewalters47 <ethanmwalters55@gmail.com>
2020-03-24 21:42:27 -0400

    initial commit

---
+++ choco-install.ps1
@@ -0,0 +1,24 @@
+choco feature enable -n allowGlobalConfirmation
+
+# Basic / Productivity
+choco install googlechrome
+choco install microsoft-teams
+choco install office365proplus
+choco install spotify
+
+# Development
+choco install notepadplusplus.install
+choco install git.install
+choco install javaruntime
+choco install jdk8
+choco install python3
+choco install filezilla
+choco install nodejs
+choco install sql-server-management-studio
+choco install vscode
+choco install winrar
+choco install mingw
+
+# System Administration
+choco install putty
+choco install virtualbox
